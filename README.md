c629e6f2d17fec49814c563d33f724b8dbd092b1
Syed Ahamed <syedahamed@Syeds-MacBook-Air.local>
2024-11-01 00:46:10 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## About
+
+This project is a clone of [Monketype](https://monkeytype.com/).
+
+### Technologies Used:
+
+- React
+- Typescript
+- Vite
+- Tailwind CSS
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="font-bold text-xl text-red-700">Hello world</h1>
+    </div>
+  );
+};
+
+export default App;
